421df14e3411d4c9c3e7df51c13745a50317bb03
kiheyunkim <kiheyunkim@gmail.com>
2022-03-01 03:11:40 +0900

    스프링 부트 종료 shell 추가

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CentOSBash
 CentOS Bash files for deploy server
+
+## List
+* kill_springboot.sh ${SERVER_PORT} : 스프링 부트 종료 후 30초 대기 / 실패시 대기 없음
---
+++ kill_springboot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+SERVER_PORT=$1
+
+PROCESS=$(netstat -nap | grep :${SERVER_PORT} | awk '{print $7}')
+
+if [ -z "${PROCESS}" ]
+then
+        echo "process not exist"
+        exit 1
+else
+        echo "find process using ${SERVER_PORT} port, PID ===> ${PROCESS}"
+fi
+
+# get PID and ProgramName with Port
+IFS="/" read -r PID PROCESS <<< "${PROCESS}"
+
+echo "trying to terminate server"
+
+#kill process
+kill -9 $PID
+
+#check process state
+for i in {1..5}; do
+        echo "standby kill process $i seconds..."
+        sleep 1
+done
+
+PROCESS=$(netstat -nap | grep :${SERVER_PORT} | awk '{print $7}')
+if [ -z "${result}" ]
+then
+        echo "process kill success"
+else
+        echo "failed to kill process"
+        exit 1
+fi
+
+echo "standby 30 seconds"
+sleep 30
+
+echo "done"
